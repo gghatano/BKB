0a841fd195a96fffd9ccd5704eec99c700b6a976
gghatano <adad0405@gmail.com>
2014-11-02 10:53:12 +0900

    ubuntuで作業

--- find_bkb.bash
+++ find_bkb.bash
@@ -10,13 +10,13 @@ time=$(date +%Y%m%d%H%M%S)
 mkdir $dir/bkb/$time
 echo $time
 
-cat $file | while read line
-do
-  filename=$(basename $file .html)
-  echo $filename
-  echo $line | 
-  $dir/bkb_judge_word.bash >> $dir/bkb/$time/bkb_word.txt.$filename
-done
+#cat $file | while read line
+#do
+#  filename=$(basename $file .html)
+#  echo $filename
+#  echo $line | 
+#  $dir/bkb_judge_word.bash >> $dir/bkb/$time/bkb_word.txt.$filename
+#done
 
 echo "START CLAUSE"
 echo $file
